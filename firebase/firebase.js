f22b517dca2e169dd22655c47402acffd2d13d68
Your Name <uf29664@gmail.com>
2024-02-03 22:04:16 +0500

    firebase

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -4,12 +4,12 @@ import {getAuth} from "firebase/auth"
 import {getFirestore} from "firebase/firestore"
 
 const firebaseConfig = {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGING_SENDER_ID,
-  appId: process.env.APP_ID,
+  apiKey:'AIzaSyD4yMgs5uinLiP0kRFhCneVk4m0ksSqf0w',
+  authDomain: "fir-auth-70a7d.firebaseapp.com",
+  projectId: "fir-auth-70a7d",
+  storageBucket: "fir-auth-70a7d.appspot.com",
+  messagingSenderId: "557649632782",
+  appId: "1:557649632782:web:a8732f3dc32200cf7fa65f",
 };
 
 const app = initializeApp(firebaseConfig);

29a0e3aeb77984ca96a3dfac1da96dad13117784
Your Name <uf29664@gmail.com>
2024-02-04 21:38:04 +0500

    firebase

--- app/login/page.js
+++ app/login/page.js
@@ -1,24 +1,23 @@
-
+'use client'
 import React, { useState } from 'react';
-import { createUserWithEmailAndPassword } from 'firebase/auth';
-import { auth } from '../../../firebase/firebase';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { auth } from '../../firebase/firebase';
 
 
-export default function AddUser() {
-    const [userName, setUserName] = useState('');
+export default function Login() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
-    const signupHandler = async () => {
+    const loginHandler = async () => {
      
 
         try { 
-            if (!email || !password || !userName) {
+            if (!email || !password) {
                 alert('Fill All Fields');
                 return;
             }
 
-            const user= await createUserWithEmailAndPassword(auth, email, password);
+            const user= await signInWithEmailAndPassword(auth, email, password);
             console.log(user);
         } catch (error) {
             console.error('Error occurred during signup:', error);
@@ -27,13 +26,7 @@ export default function AddUser() {
   return (
     <div>
     <form onSubmit={(e)=>e.target.value}>
-        <input
-            className='border-red-500 px-10 border-2 my-5'
-            type="text"
-            onChange={(e) => setUserName(e.target.value)}
-            placeholder='Enter Name'
-            required
-        /><br />
+    
         <input
             className='border-red-500 px-10 border-2 my-5'
             type="email"
@@ -48,7 +41,7 @@ export default function AddUser() {
             onChange={(e) => setPassword(e.target.value)}
             required
         /><br />
-        <button onClick={signupHandler} className='bg-blue-400 font-bold py-2 px-3 rounded-lg hover:text-white'>Sign Up</button>
+        <button onClick={loginHandler} className='bg-blue-400 font-bold py-2 px-3 rounded-lg hover:text-white'>login</button>
     </form>
 </div>
   )

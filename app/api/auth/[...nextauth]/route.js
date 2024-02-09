50818c66259ef24cad4e46fc5e82f8fa84b21c8b
Umar Farooq <uf29664@gmail.com>
2024-02-09 09:30:59 -0800

    firebase

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -3,19 +3,21 @@ import NextAuth from "next-auth/next";
 import CredentialsProvider from "next-auth/providers/credentials";
 import GoogleProvider from "next-auth/providers/google";
 const options ={
+  secret :process.env.SECRET,
+ 
 
     providers: [
   CredentialsProvider({
     name: "Credentials",
   
     credentials: {
-      username: { label: "Username", type: "text", placeholder: "jsmith" },
+      username: { label: "Email", type: "email", placeholder: "email" },
       password: { label: "Password", type: "password" }
     },
     async authorize(credentials, req) {
         console.log("credentials",credentials);
       // Add logic here to look up the user from the credentials supplied
-      const user = { id: "1", name: "J Smith", email: "jsmith@example.com" }
+      const user = { id: "1", name: "Umar", email: "uf29664@gmail.com" }
 
       if (user) {
         return user

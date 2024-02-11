bf839d7a0ff5a68b4927f69de8aca68ce7b16f54
Umar Farooq <uf29664@gmail.com>
2024-02-10 21:38:00 -0800

    firebase

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,5 +1,7 @@
 import NextAuth from "next-auth/next";
 
+import {auth} from '@/firebase/firebase'
+import { signInWithEmailAndPassword } from 'firebase/auth';
 import CredentialsProvider from "next-auth/providers/credentials";
 import GoogleProvider from "next-auth/providers/google";
 const options ={
@@ -14,18 +16,24 @@ const options ={
       username: { label: "Email", type: "email", placeholder: "email" },
       password: { label: "Password", type: "password" }
     },
-    async authorize(credentials, req) {
-        console.log("credentials",credentials);
-      // Add logic here to look up the user from the credentials supplied
-      const user = { id: "1", name: "Umar", email: "uf29664@gmail.com" }
+    async authorize(credentials) {
+      try {
+        // Sign in with Firebase Authentication
+        const userCredential = await signInWithEmailAndPassword(auth, credentials.email, credentials.password);
+        const user = userCredential.user;
+        console.log(userCredential);
 
-      if (user) {
-        return user
-      } else {
-        return null
-
+        // Return the user object if authentication is successful
+        if (user) {
+          return { id: user.uid, email: user.email };
+        } else {
+          return null; // Return null if authentication fails
+        }
+      } catch (error) {
+        console.error('Authentication error:', error);
+        return null; // Return null if an error occurs
       }
-    }
+    },
   }),
   GoogleProvider({
     clientId: process.env.GOOGLE_CLIENT_ID,

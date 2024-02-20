18524e0db4c55c8024675d1dec2b7cf134090689
Umar Farooq <uf29664@gmail.com>
2024-02-20 09:12:21 -0800

    firebse

--- app/components/navbar/Navbar.js
+++ app/components/navbar/Navbar.js
@@ -1,9 +1,10 @@
 'use client'
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import { IoLogoCodepen } from "react-icons/io";
 import Link from 'next/link';
 import { IoMenu } from "react-icons/io5";
 import { IoMdClose } from "react-icons/io";
+
 export default function Navbar() {
   let Links = [
     { name: 'Home', link: "/" },
@@ -12,32 +13,35 @@ export default function Navbar() {
     { name: 'Blog', link: "/" },
   ]
 
-  const [open,setOpen]=useState(false)
-  const toggleMenu = ()=>{
-    setOpen(!open)
-  }
+  const [open, setOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setOpen(!open);
+  };
+
   return (
     <div>
-    <nav className='md:flex justify-between items-center px-10 pt-4 lg:bg-slate-400 md:bg-red-300 sm:bg-green-200'>
-      <div className='flex justify-center items-center md:static sm:absolute sm:left-2 sm:top-3'>
-        <IoLogoCodepen className='color-blue-400 text-5xl pr-2 ' />
-        <h1 className='text-xl font-bold'>Umar Farooq</h1>
+      <nav className='md:flex justify-between items-center px-10 pt-4 lg:bg-slate-400 md:bg-red-300 sm:bg-green-200'>
+        <div className='flex justify-center items-center md:static sm:absolute sm:left-2 sm:top-3'>
+          <IoLogoCodepen className='color-blue-400 text-5xl pr-2 ' />
+          <h1 className='text-xl font-bold'>Umar Farooq</h1>
+        </div>
+        <ul className={`md:flex sm:pt-12 md:pt-0 text-xl font-bold ${open ? 'top-20 opacity-100' : 'top-[-100px] opacity-0'} md:opacity-100 sm:absolute md:relative transition-all duration-500 ease-in-out`}>
+          {Links.map((link) => (
+            <li className='hover:bg-black rounded-3xl sm:mt-5 md:mt-0  py-2 px-6 ml-3 hover:text-white sm:text-center' key={link.name}>
+              <Link href={link.link}>{link.name}</Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+      <div onClick={toggleMenu}>
+        {open ? (
+          <IoMdClose className='text-3xl absolute right-5 top-6 md:hidden' />
+        ) : (
+          <IoMenu className='text-3xl absolute right-5 top-6 md:hidden' />
+        )}
       </div>
-      <ul className={`md:flex sm:pt-12 md:pt-0 text-xl font-bold ${open ? 'top-20 opacity-100' : 'top-[-300px]'} md:opacity-100  `}>
-        {Links.map((link) => (
-          <li className='hover:bg-black rounded-3xl sm:mt-5 md:mt-0  py-2 px-6 ml-3 hover:text-white sm:text-center' key={link.name}>
-            <Link href={link.link}>{link.name}</Link>
-          </li>
-        ))}
-      </ul>
-    </nav>
-    <div onClick={toggleMenu}>
-      {open ? (
-        <IoMdClose className='text-3xl absolute right-5 top-6 md:hidden' />
-      ) : (
-        <IoMenu className='text-3xl absolute right-5 top-6 md:hidden' />
-      )}
     </div>
-  </div>
-  )
+  );
 }
+
--- app/page.js
+++ app/page.js
@@ -8,6 +8,14 @@ export default function Home() {
  <div>
  
  <Navbar/>
+ <div className='bg-pink-300 mt-14'>
+  <div>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio alias ipsum recusandae, tempora dignissimos rem id enim quis dolor nihil ad aliquam praesentium cupiditate autem dicta veniam ea reprehenderit blanditiis!</p>
+  </div>
+
+ </div>
+
+ 
  
  </div>
   );

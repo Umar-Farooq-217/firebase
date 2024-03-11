acc8f003c66bcb708594d547e8cf57ecc4db789f
Umar Farooq <uf29664@gmail.com>
2024-03-10 22:35:33 -0700

    firebase

---
+++ app/practice/page.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../../app/globals.css'
+
+export default function page() {
+  return (
+    <div>
+      <button className=" mt-10 bg-green-500 py-3 btn px-4">click</button>
+    </div>
+  )
+}
--- app/signup/page.js
+++ app/signup/page.js
@@ -29,7 +29,7 @@ export default function SignUpPage() {
                   <label for="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
                   <input type="password" name="password" id="password" onChange={(e)=>setPassword(e.target.value)} placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required=""/>
                 </div>
-                <div></div>
+                
                
                 <div className="flex items-start">
                   

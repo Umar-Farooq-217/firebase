48c51152802ef996f270be6d2625649a82db640c
Umar Farooq <uf29664@gmail.com>
2024-03-12 15:37:01 -0700

    firebase

--- app/practice/page.js
+++ app/practice/page.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import '../../app/globals.css'
+import Image from 'next/image'
 
 export default function page() {
   return (
     <div>
       <button className=" mt-10 bg-green-500 py-3 btn px-4">click</button>
+
+      <button className="ml-10 bg-red-600 text-xl p-3 anima ">Users</button>
+
+      <Image width={100}  height={100}  src='/flower.avif' className='bg-white flex m-auto rotation' />
     </div>
   )
 }

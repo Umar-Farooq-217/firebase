3c49dd434d71592a4ad5f0248f41bfcac666d0d6
Umar Farooq <uf29664@gmail.com>
2024-03-12 22:29:17 -0700

    firebase

--- app/practice/page.js
+++ app/practice/page.js
@@ -10,6 +10,12 @@ export default function page() {
       <button className="ml-10 bg-red-600 text-xl p-3 anima ">Users</button>
 
       <Image width={100}  height={100}  src='/flower.avif' className='bg-white flex m-auto rotation' />
+
+      <div className="flex m-auto">
+        <button className="px-7 py-2 text-white font-bold colors flex m-auto mt-7">Colors</button>
+      </div>
+<div className="w-10 h-10 square"></div>
+
     </div>
   )
 }

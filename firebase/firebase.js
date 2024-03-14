01ba7b784c82273f313dde4c106f503e2b9bd32f
Umar Farooq <uf29664@gmail.com>
2024-03-13 22:40:43 -0700

    firebase

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -4,7 +4,12 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
- 
+    apiKey: "AIzaSyCWt2eSlFp8D_QRtYXHj1pHxR4bcya-0CM",
+    authDomain: "authentification-29c29.firebaseapp.com",
+    projectId: "authentification-29c29",
+    storageBucket: "authentification-29c29.appspot.com",
+    messagingSenderId: "895699538051",
+    appId: "1:895699538051:web:1aa6b7db0f5fdc1bc34bb6"
 };
 
 const app = initializeApp(firebaseConfig);

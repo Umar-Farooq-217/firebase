5fc0af79f72bc95b055c31a2aa5a1df4e2b42d62
Umar Farooq <uf29664@gmail.com>
2024-04-07 14:15:44 -0700

    firebase

--- app/model/model.js
+++ app/model/model.js
@@ -1,2 +1,10 @@
 import mongoose from "mongoose";
 const {Schema}=mongoose
+
+const userSchema = new Schema({
+    firstName:{type:String,required:true},
+    lastName:String,
+    email:{type:String,required:true},
+    phone:Int
+
+})
